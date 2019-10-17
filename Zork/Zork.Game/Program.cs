fd5d532574c9601dda2784f74da5582fff6723ad
CarltonRagsdale <carltonragsdale@knights.ucf.edu>
2019-10-17 19:05:23 -0400

    CreatedTemplateForZorkDesigner

---
+++ Zork/Zork.Game/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Zork.Common;
+
+namespace Zork.Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            const string defaultGameFilename = "Rooms.json";
+            string gameFilename = (args.Length > 0 ? args[(int)CommandLineArguments.GameFilename] : defaultGameFilename);
+
+            Common.Game game = Common.Game.Load(gameFilename);
+            Console.WriteLine("Welcome to Zork!");
+            game.Run();
+            Console.WriteLine("Thanks for playing!");
+        }
+        private enum CommandLineArguments
+        {
+            GameFilename = 0
+        }
+    }
+}
